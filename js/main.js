9e1532d9815f59bf7e64850f05feddff66d829f0
Danilo <danilo.colloca91@gmail.com>
2021-01-13 17:18:46 +0100

    .keydown per muovere le foto con le frecce della tastiera

--- js/main.js
+++ js/main.js
@@ -2,8 +2,14 @@
 
 $(document).ready(
   function(){
+    //rendo standard la dimensione delle immagini
+    $('img').css({
+      'width': '1280px'
+    })
 
-    $('.next').click(//se clicco sull'elemento con class 'next'
+/*+++++++++++cambio foto cliccando sulle frecce con mouse++++++++++++++*/
+
+    $('.next').click(//quando clicco sull'elemento con class 'next'
       function(){
    /* selezionoImgClassActive. rimuovoClass'active'. passoAll'elementoPiùVicino. aggiungoClassActive*/
         $('img.active').removeClass('active').next().addClass('active')
@@ -12,12 +18,17 @@ $(document).ready(
       function(){
         $('img.active').removeClass('active').prev().addClass('active')
     })
-
-    //rendo standard la dimensione delle immagini 
-    $('img').css({
-      'width': '1280px'
-
-    })
-
+/*+++++++++++cambio foto con frecce tastiera++++++++++++++*/
+/*quando nel DOM. premiamo un tasto si attiva la funzione, con argomento 'x' che è dato da .keydown*/
+  $(document).keydown(function(keyEvent) {
+//se keydown rileva keyevent che corrisponde a .keyCode 39
+      if (keyEvent.keyCode === 39) {
+//allora div class .next
+         $(".next").click();
+      }
+      if (keyEvent.keyCode === 37) {
+         $(".prev").click();
+      }
+  });
 
 })
