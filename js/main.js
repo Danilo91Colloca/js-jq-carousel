028b0906a1c5dd352f3d9247a97f3e0b827c36e8
Danilo <danilo.colloca91@gmail.com>
2021-01-13 20:23:52 +0100

    ho agganciato i pallini allo scorrimento delle immagini

--- js/main.js
+++ js/main.js
@@ -5,22 +5,39 @@ $(document).ready(
     //rendo standard la dimensione delle immagini
     $('img').css({
       'width': '1280px'
-    })
+    });
 
 /*+++++++++++cambio foto cliccando sulle frecce con mouse++++++++++++++*/
-
     $('.next').click(//quando clicco sull'elemento con class 'next'
       function(){
    /* selezionoImgClassActive. rimuovoClass'active'. passoAll'elementoPiùVicino. aggiungoClassActive*/
-        $('img.active').removeClass('active').next().addClass('active')
-    })
-    $('.prev').click(
-      function(){
-        $('img.active').removeClass('active').prev().addClass('active')
-    })
+      $('img.active')
+      .removeClass('active')
+      .next().addClass('active');
+      $('i.active')
+      .removeClass('active')
+      .next().addClass('active');
+
+
+
+   })
+
+   $('.prev').click(
+     function(){
+       $('img.active')
+       .removeClass('active')
+       .prev().addClass('active')
+       $('i.active')
+       .removeClass('active')
+       .prev().addClass('active');;
+   })
+
+
+
 /*+++++++++++cambio foto con frecce tastiera++++++++++++++*/
 /*quando nel DOM. premiamo un tasto si attiva la funzione, con argomento 'x' che è dato da .keydown*/
-  $(document).keydown(function(keyEvent) {
+  $(document).keydown(
+    function(keyEvent) {
 //se keydown rileva keyevent che corrisponde a .keyCode 39
       if (keyEvent.keyCode === 39) {
 //allora div class .next
@@ -31,4 +48,18 @@ $(document).ready(
       }
   });
 
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})//chiusura ready
